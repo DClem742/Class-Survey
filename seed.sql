29e61a6db5e45417be81aecdf163979d23744ebd
DC Isenberg <dclem742@gmail.com>
2024-10-04 14:07:07 -0400

    created database

---
+++ seed.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO topics (topic_name) VALUES
+('HTML'),
+('CSS'),
+('JavaScript');
+
+INSERT INTO rankings (ranking_name) VALUES
+('Awesome'),
+('Great'),
+('Good'),
+('Poor'),
+('Unranked');
